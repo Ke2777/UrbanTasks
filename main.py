889ce0ba32ea6c1b0026124fa4f5971c2e073a5e
Ke2777 <74142541+Ke2777@users.noreply.github.com>
2024-06-26 13:01:09 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+#task 1
+a = "aeljcgaeeeljcgadlj"
+print("длинна строки: ", len(a))
+
+#task 2
+first = 13
+second = 7
+summa = first + second
+diff = first - second
+print("сумма чисел: ", summa,"разность чисел: ", diff)
+
+#task 3
+first = 10
+second = 18
+third = 35
+mean = (first + second + third)/3
+print(mean)
+
+#task 4
+first_string = "вторник"
+second_string = "понедельник"
+print(second_string, ", ", first_string)
+
+#task 5
+a = 23
+b = 0
+c = 2
+
+f = (a * b) + (a * c)
+print((f**3)/2)
