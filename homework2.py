90a2b232ebcc59129846e5ea992cb331414261be
Ke2777 <74142541+Ke2777@users.noreply.github.com>
2024-07-01 11:46:39 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+complite_tasks = 12
+hours = 1.5
+course_name = 'Python'
+hours_to_complite = hours/complite_tasks
+print('Курс:', course_name, 'всего задач:', complite_tasks, 'затрачено часов:', hours, 'среднее время выполнения:', hours_to_complite)
