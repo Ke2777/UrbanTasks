045185b7a5ce587341a71bc5376548c5a415c4b5
Ke2777 <74142541+Ke2777@users.noreply.github.com>
2024-07-05 17:42:22 +0300

    module_2_2 - done

---
+++ module_2_2.py
@@ -0,0 +1,12 @@
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
+result = 0
+
+if first == second == first: result = 3
+else:
+    if first == second: result += 2
+    if second == third: result += 2
+    if third == first: result += 2
+
+print(result)
