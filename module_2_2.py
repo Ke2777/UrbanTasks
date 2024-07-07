ae67bf31eceeb8928af159d34987b4486334cdb9
Ke2777 <74142541+Ke2777@users.noreply.github.com>
2024-07-08 00:02:24 +0300

    module_2_2 fixed
    Исправлена опечатка в коде и функционал программы

--- module_2_2.py
+++ module_2_2.py
@@ -3,7 +3,7 @@ second = int(input('Введите второе число: '))
 third = int(input('Введите третье число: '))
 result = 0
 
-if first == second == first: result = 3
+if first == second == third: result = 3
 else:
     if first == second: result += 2
     if second == third: result += 2
