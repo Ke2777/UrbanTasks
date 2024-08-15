19a760fd3715f6a27fe82d6e22ded7671e6b7acf
Ke2777 <kir@ya.ru>
2024-08-15 21:47:00 +0300

    module_5_3 - done

--- homework2.py
+++ homework2.py
@@ -3,5 +3,3 @@ hours = 1.5
 course_name = 'Python'
 hours_to_complite = hours/complite_tasks
 print('Курс:', course_name, 'всего задач:', complite_tasks, 'затрачено часов:', hours, 'среднее время выполнения:', hours_to_complite)
-
-# Github работай!!!
