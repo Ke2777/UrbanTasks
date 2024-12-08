d0af14dd1023c5c48ae0943835aa6fcea6e1a254
Ke2777 <kir@ya.ru>
2024-12-08 14:44:00 +0300

    module_13_1 - done

---
+++ module_13_1.py
@@ -0,0 +1,19 @@
+import asyncio
+
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования.')
+    for i in range(1, 6):
+        await asyncio.sleep(1 / power)
+        print(f"Силач {name} поднял {i} шар")
+    print(f'Силач {name} закончил соревнования.')
+
+
+async def start_tournament():
+    task1 = asyncio.create_task(start_strongman('Олег', 2))
+    task2 = asyncio.create_task(start_strongman('Мустанг', 5))
+    task3 = asyncio.create_task(start_strongman('Машина', 1))
+    await asyncio.gather(task1, task2, task3)
+
+if __name__ == '__main__':
+    asyncio.run(start_tournament())
