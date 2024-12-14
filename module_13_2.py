fe3faf29e636154ce79835c733f02f2f6d628b15
Ke2777 <kir@ya.ru>
2024-12-14 04:18:43 +0300

    module_13_2 - changed

--- module_13_2.py
+++ module_13_2.py
@@ -1,25 +1,21 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
-from aiogram.utils import executor
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.files import MemoryStorage
+import asyncio
 
-API_TOKEN = ''
-
+API_TOKEN = ""
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
 @dp.message_handler(commands=['start'])
-async def start(message: Message):
-    print("Привет! Я бот помогающий твоему здоровью.")
-    await message.reply("Привет! Я бот помогающий твоему здоровью.")
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
 
 
 @dp.message_handler()
-async def all_messages(message: Message):
-    print("Введите команду /start, чтобы начать общение.")
-    await message.reply("Введите команду /start, чтобы начать общение.")
+async def all_massages(message):
+    print('Введите команду /start, чтобы начать общение.')
 
 
 if __name__ == '__main__':
-    print("Bot is running...")
     executor.start_polling(dp, skip_updates=True)
