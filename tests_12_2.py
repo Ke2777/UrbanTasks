d43edb6c0da8164eb091bf7509275ead1da18c5f
Ke2777 <kir@ya.ru>
2024-12-20 01:49:56 +0300

    tests 12_2 - completed

--- tests_12_2.py
+++ tests_12_2.py
@@ -3,11 +3,11 @@ from module_12_2 import Runner, Tournament
 
 
 def skip_if_frozen(test_func):
-    """Декоратор для пропуска тестов, если is_frozen = True."""
     def wrapper(self, *args, **kwargs):
         if getattr(self.__class__, 'is_frozen', False):
             self.skipTest("Тесты в этом кейсе заморожены")
         return test_func(self, *args, **kwargs)
+
     return wrapper
 
 
@@ -25,8 +25,8 @@ class RunnerTest(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        for result in cls.all_results.values():
-            print(result)
+        for result_name, results in cls.all_results.items():
+            print(f"Результаты для {result_name}: {results}")
 
     @skip_if_frozen
     def test_walk(self):
@@ -42,15 +42,16 @@ class RunnerTest(unittest.TestCase):
             runner.run()
         self.assertEqual(runner.distance, 100)
 
-    @skip_if_frozen
-    def test_challenge(self):
-        tournament = Tournament(90, self.runner_usain, self.runner_nick)
-        results = tournament.start()
-        self.assertTrue(results[max(results)].name == "Ник")
+    # @skip_if_frozen
+    # def test_challenge(self):
+    #     tournament = Tournament(90, self.runner_usain, self.runner_nick)
+    #     results = tournament.start()
+    #     self.__class__.all_results["Турнир Усэйн и Ник"] = {place: str(runner) for place, runner in results.items()}
+    #     self.assertTrue(results[max(results)].name == "Ник")
 
 
 class TournamentTest(unittest.TestCase):
-    is_frozen = True
+    is_frozen = False
 
     @classmethod
     def setUpClass(cls):
@@ -63,25 +64,28 @@ class TournamentTest(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        for result in cls.all_results.values():
-            print(result)
+        for result_name, results in cls.all_results.items():
+            print(f"Результаты для {result_name}: {results}")
 
     @skip_if_frozen
     def test_first_tournament(self):
         tournament = Tournament(90, self.runner_usain, self.runner_nick)
         results = tournament.start()
+        self.__class__.all_results["Турнир Усэйн и Ник"] = {place: str(runner) for place, runner in results.items()}
         self.assertTrue(results[max(results)].name == "Ник")
 
     @skip_if_frozen
     def test_second_tournament(self):
         tournament = Tournament(90, self.runner_andrei, self.runner_nick)
         results = tournament.start()
+        self.__class__.all_results["Турнир Андрей и Ник"] = {place: str(runner) for place, runner in results.items()}
         self.assertTrue(results[max(results)].name == "Ник")
 
     @skip_if_frozen
     def test_third_tournament(self):
         tournament = Tournament(90, self.runner_usain, self.runner_andrei, self.runner_nick)
         results = tournament.start()
+        self.__class__.all_results["Турнир Усэйн, Андрей и Ник"] = {place: str(runner) for place, runner in results.items()}
         self.assertTrue(results[max(results)].name == "Ник")
 
 
