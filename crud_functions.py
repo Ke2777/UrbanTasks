8cd63be3918a4aa24c410fc05c8b9fe36cda57d4
Ke2777 <kir@ya.ru>
2024-12-22 02:45:51 +0300

    module_14_5 - completed

--- crud_functions.py
+++ crud_functions.py
@@ -4,17 +4,24 @@ import sqlite3
 def initiate_db():
     conn = sqlite3.connect('products.db')
     cursor = conn.cursor()
+    # Создание таблицы Products
     cursor.execute('''CREATE TABLE IF NOT EXISTS Products (
                         id INTEGER PRIMARY KEY,
                         title TEXT NOT NULL,
                         description TEXT,
                         price INTEGER NOT NULL
                     )''')
+    # Создание таблицы Users
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Users (
+                        id INTEGER PRIMARY KEY,
+                        username TEXT NOT NULL,
+                        email TEXT NOT NULL,
+                        age INTEGER NOT NULL,
+                        balance INTEGER NOT NULL DEFAULT 1000
+                    )''')
     conn.commit()
     conn.close()
 
-
-# Заполнение таблицы продуктами
 
 def fill_products():
     conn = sqlite3.connect('products.db')
@@ -39,3 +46,21 @@ def get_all_products():
     products = cursor.fetchall()
     conn.close()
     return products
+
+
+def add_user(username, email, age):
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    cursor.execute('''INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, 1000)''',
+                   (username, email, age))
+    conn.commit()
+    conn.close()
+
+
+def is_included(username):
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT 1 FROM Users WHERE username = ?', (username,))
+    result = cursor.fetchone()
+    conn.close()
+    return result is not None
---
+++ module_14_5.py
@@ -0,0 +1,175 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.files import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from crud_functions import initiate_db, fill_products, get_all_products, add_user, is_included
+import sqlite3
+
+API_TOKEN = "7360248683:AAElcUNNzBiDC-hv3rcsCjA7cH6y0zquQeY"
+health_bot = Bot(token=API_TOKEN)
+dispatcher = Dispatcher(health_bot, storage=MemoryStorage())
+
+main_menu = ReplyKeyboardMarkup(resize_keyboard=True)
+inline_menu = InlineKeyboardMarkup()
+
+calorie_calc_button = InlineKeyboardButton(text="Рассчитать норму калорий", callback_data="calculate_calories")
+formulas_button = InlineKeyboardButton(text="Формулы расчёта", callback_data="show_formulas")
+
+inline_menu.add(calorie_calc_button)
+inline_menu.add(formulas_button)
+
+calculate_button = KeyboardButton(text="Рассчитать")
+info_button = KeyboardButton(text="Информация")
+buy_button = KeyboardButton(text="Купить")
+register_button = KeyboardButton(text="Регистрация")
+
+main_menu.add(calculate_button)
+main_menu.add(info_button)
+main_menu.add(buy_button)
+main_menu.add(register_button)
+
+inline_menu_products = InlineKeyboardMarkup()
+for i in range(1, 5):
+    inline_menu_products.add(InlineKeyboardButton(text=f'Product{i}', callback_data="product_buying"))
+
+
+class UserInput(StatesGroup):
+    age_state = State()
+    height_state = State()
+    weight_state = State()
+
+
+class RegistrationState(StatesGroup):
+    username = State()
+    email = State()
+    age = State()
+
+
+@dispatcher.message_handler(commands=["start"])
+async def start_conversation(message: types.Message):
+    await message.answer("Привет! Я бот, помогающий твоему здоровью.", reply_markup=main_menu)
+
+
+@dispatcher.message_handler(text="Рассчитать")
+async def show_options(message: types.Message):
+    await message.answer("Выберите опцию:", reply_markup=inline_menu)
+
+
+@dispatcher.callback_query_handler(text="show_formulas")
+async def display_formulas(callback_query: types.CallbackQuery):
+    await callback_query.message.answer("10 x вес + 6.25 х рост - 5 * возраст + 5")
+    await callback_query.answer()
+
+
+@dispatcher.callback_query_handler(text="calculate_calories")
+async def request_age(callback_query: types.CallbackQuery):
+    await callback_query.message.answer("Введите свой возраст:")
+    await UserInput.age_state.set()
+
+
+@dispatcher.message_handler(state=UserInput.age_state)
+async def request_height(message: types.Message, state: FSMContext):
+    await state.update_data(age=int(message.text))
+    await message.answer("Введите свой рост:")
+    await UserInput.height_state.set()
+
+
+@dispatcher.message_handler(state=UserInput.height_state)
+async def request_weight(message: types.Message, state: FSMContext):
+    await state.update_data(height=int(message.text))
+    await message.answer("Введите свой вес:")
+    await UserInput.weight_state.set()
+
+
+@dispatcher.message_handler(state=UserInput.weight_state)
+async def calculate_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=int(message.text))
+    user_data = await state.get_data()
+    calorie_norm = (10 * user_data["weight"]) + (6.25 * user_data["height"]) - (5 * user_data["age"]) + 5
+    await message.answer(f"Ваша норма по количеству калорий в сутки: {calorie_norm}")
+    await state.finish()
+
+
+@dispatcher.message_handler(text='Купить')
+async def get_buying_list(message: types.Message):
+    products = [
+        {"name": "Product 1", "desc": "Описание 1", "price": 100,
+         "img": "https://marketguru.io/secrets/wp-content/uploads/2023/05/3-2-768x511.jpg"},
+        {"name": "Product 2", "desc": "Описание 2", "price": 200,
+         "img": "https://www.mos.ru/upload/newsfeed/newsfeed/onlineshopping(9).jpg"},
+        {"name": "Product 3", "desc": "Описание 3", "price": 300,
+         "img": "https://alev.biz/wp-content/uploads/2021/11/online-shopping.jpg"},
+        {"name": "Product 4", "desc": "Описание 4", "price": 400,
+         "img": "https://thumbs.dreamstime.com/b/магазинная-тележкаа-28651333.jpg"}
+    ]
+
+    for product in products:
+        await message.answer_photo(
+            photo=product["img"],
+            caption=f'Название: {product["name"]} | {product["desc"]} | Цена: {product["price"]}'
+        )
+
+    await message.answer("Выберите продукт для покупки:", reply_markup=inline_menu_products)
+
+
+@dispatcher.callback_query_handler(text="product_buying")
+async def send_confirm_message(callback_query: types.CallbackQuery):
+    await callback_query.message.answer("Вы успешно приобрели продукт!")
+    await callback_query.answer()
+
+
+@dispatcher.message_handler(text="Регистрация")
+async def sign_up(message: types.Message):
+    await RegistrationState.username.set()
+    await message.reply("Введите имя пользователя (только латинский алфавит):")
+
+
+@dispatcher.message_handler(state=RegistrationState.username)
+async def set_username(message: types.Message, state: FSMContext):
+    username = message.text
+    if is_included(username):
+        await message.reply("Пользователь существует, введите другое имя:")
+    else:
+        await state.update_data(username=username)
+        await RegistrationState.next()
+        await message.reply("Введите свой email:")
+
+
+@dispatcher.message_handler(state=RegistrationState.email)
+async def set_email(message: types.Message, state: FSMContext):
+    email = message.text
+    await state.update_data(email=email)
+    await RegistrationState.next()
+    await message.reply("Введите свой возраст:")
+
+
+@dispatcher.message_handler(state=RegistrationState.age)
+async def set_age(message: types.Message, state: FSMContext):
+    age = message.text
+    if not age.isdigit():
+        await message.reply("Пожалуйста, введите корректный возраст:")
+        return
+
+    age = int(age)
+    data = await state.get_data()
+    username = data['username']
+    email = data['email']
+
+    add_user(username, email, age)
+    await message.reply("Регистрация прошла успешно!")
+    await state.finish()
+
+
+@dispatcher.message_handler()
+async def all_messages(message: types.Message):
+    await message.answer(
+        "Извините, я не понял вашего запроса. Пожалуйста, используйте меню для взаимодействия со мной.",
+        reply_markup=main_menu)
+
+
+if __name__ == "__main__":
+    initiate_db()
+    fill_products()
+    executor.start_polling(dispatcher, skip_updates=True)
